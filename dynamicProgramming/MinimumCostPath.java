7ce1cfd74888bd151dfa2b27940c4b4ab7e4b53f
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-06 18:09:25 +0530

    done with minimum costmemo

--- dynamicProgramming/MinimumCostPath.java
+++ dynamicProgramming/MinimumCostPath.java
@@ -47,6 +47,47 @@ public class MinimumCostPath {
 		int myAns = cost[i][j] + Math.min(ans1, Math.min(ans2, ans3));
 		return myAns;
 	}
+	
+	public static int minCostPathReMemo(int[][]cost, int [][] dp) {
+		return minCostPathReMemoHelper(cost, dp, 0, 0);
+	}
+	public static int minCostPathReMemoHelper(int[][] cost, int[][] dp,  int i, int j ) {
+		int m = cost.length;
+		int n = cost[0].length;
+//		special case
+		if(i == m - 1 && j == n - 1) {
+			return cost[i][j];
+		}
+//		Base case
+		if(i >= n || j >= n) {
+			return Integer.MAX_VALUE;
+		}
+		int ans1, ans2, ans3;
+		if(dp[i + 1][j] == Integer.MIN_VALUE) {
+			ans1 = minCostPathReHelper(cost, i + 1, j);
+			dp[i + 1][j] = ans1;
+		}else {
+			ans1 = dp[i + 1][j];
+		}
+		
+		if(dp[i][j + 1] == Integer.MIN_VALUE) {
+			ans2 = minCostPathReHelper(cost, i, j + 1);
+			dp[i][j + 1] = ans2;
+		}else {
+			ans2 = dp[i][j + 1];
+		}
+		
+		if(dp[i + 1][j + 1] == Integer.MIN_VALUE) {
+			ans3 = minCostPathReHelper(cost, i + 1, j + 1);
+			dp[i + 1][j + 1] = ans3;
+		}else {
+			ans3 = dp[i + 1][j + 1];
+		}
+		
+		
+		int myAns = cost[i][j] + Math.min(ans1, Math.min(ans2, ans3));
+		return myAns;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
@@ -58,7 +99,15 @@ public class MinimumCostPath {
 				cost[i][j] = s.nextInt();
 			}
 		}
-		System.out.println(minCostPathRe(cost));
+		
+		int[][] dp = new int[cost.length + 1][cost[0].length + 1];
+		for(int i = 0; i < dp.length; i++) {
+			for(int j = 0; j < dp[0].length; j++) {
+				dp[i][j] = Integer.MIN_VALUE;
+			}
+		}
+//		System.out.println(minCostPathRe(cost));
+		System.out.println(minCostPathReMemo(cost, dp));
 	}
 
 
