a36af018d74946f581ddf4d993527085a5e84c61
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-06 20:10:37 +0530

    done with minimum cost iterative

--- dynamicProgramming/MinimumCostPath.java
+++ dynamicProgramming/MinimumCostPath.java
@@ -88,6 +88,33 @@ public class MinimumCostPath {
 		int myAns = cost[i][j] + Math.min(ans1, Math.min(ans2, ans3));
 		return myAns;
 	}
+	
+	public static int minimumCostIterative(int[][] cost) {
+		int m = cost.length;
+		int n = cost[0].length;
+		
+		int[][] dp = new int[m + 1][n + 1];
+		
+		for(int i = 0; i < dp.length;i++) {
+			for(int j = 0; j < dp[i].length; j++) {
+				dp[i][j] = Integer.MAX_VALUE;
+			}
+		}
+		
+		for(int i = m - 1; i >= 0; i--) {
+			for(int j = n - 1; j >= 0; j--) {
+				if(i == m - 1 && j == n - 1) {
+					dp[i][j] = cost[i][j];
+					continue;
+				}
+				int ans1 = dp[i][j + 1];
+				int ans2 = dp[i + 1][j];
+				int ans3 = dp[i + 1][j + 1];
+				dp[i][j] = cost[i][j] + Math.min(ans1, Math.min(ans2, ans3));
+			}
+		}
+		return dp[0][0];
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
@@ -107,7 +134,8 @@ public class MinimumCostPath {
 			}
 		}
 //		System.out.println(minCostPathRe(cost));
-		System.out.println(minCostPathReMemo(cost, dp));
+//		System.out.println(minCostPathReMemo(cost, dp));
+		System.out.println(minimumCostIterative(cost));
 	}
 
 
