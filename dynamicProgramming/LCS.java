2d04e84f35aa31a80494db02c1419d79f1fa8649
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-13 17:33:21 +0530

    done with LCSITER

--- dynamicProgramming/LCS.java
+++ dynamicProgramming/LCS.java
@@ -85,19 +85,44 @@ public class LCS {
 		}
 		return myAns;
 	}
+	
+	public static int LCSI(String str1, String str2) {
+		int m = str1.length();
+		int n = str2.length();
+		int[][] dp = new int[m + 1][n + 1];
+		for(int i = m - 1; i >= 0; i--) {
+			for(int j = n - 1; j >= 0; j--) {
+				
+				int ans;
+				if(str1.charAt(i) == str2.charAt(j)) {
+					ans = 1 + dp[i + 1][j + 1];
+					
+				}else {
+					int ans1 = dp[i][j + 1];
+					int ans2 = dp[i + 1][j];
+					ans = Math.max(ans1, ans2);
+					
+					
+				}
+				dp[i][j] = ans;
+			}
+		}
+		return dp[0][0];
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		String str1 = s.nextLine();
 		String str2 = s.nextLine();
 		//		System.out.println(lcs(str1, str2));
-		int[][] dp = new int[(str1.length() + 1)][str2.length() + 1];
-		for(int i = 0; i < dp.length; i++) {
-			for(int j = 0; j < dp[0].length; j++) {
-				dp[i][j] = -1;
-			}
-		}
-		System.out.println(LCSRECMEMO(str1, str2, dp));
+//		int[][] dp = new int[(str1.length() + 1)][str2.length() + 1];
+//		for(int i = 0; i < dp.length; i++) {
+//			for(int j = 0; j < dp[0].length; j++) {
+//				dp[i][j] = -1;
+//			}
+//		}
+//		System.out.println(LCSRECMEMO(str1, str2, dp));
+		System.out.println(LCSI(str1, str2));
 	}
 
 }
