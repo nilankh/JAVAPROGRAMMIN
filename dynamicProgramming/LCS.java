dec3f50e7c22df7ca728bd404684c4ffadd2e852
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-13 16:23:39 +0530

    done with memo of lCS

--- dynamicProgramming/LCS.java
+++ dynamicProgramming/LCS.java
@@ -21,18 +21,18 @@
 package dynamicProgramming;
 import java.util.Scanner;
 public class LCS {
-	
-	
+
+
 	public static int lcs(String s1, String s2) {
 		return lcsHelper(s1, s2, 0, 0);
 	}
-	
+
 	public static int lcsHelper(String str1, String str2, int i, int j) {
-		
+
 		if(i == str1.length() || j == str2.length()) {
 			return 0;
 		}
-		
+
 		int myAns;
 		if(str1.charAt(i) == str2.charAt(j)) {
 			int smallAns = lcsHelper(str1, str2, i + 1, j + 1);
@@ -44,12 +44,60 @@ public class LCS {
 		}
 		return myAns;
 	}
+
+
+
+	public static int LCSRECMEMO(String s1, String s2, int[][] dp) {
+		return LCSRECMEMOHelper(s1, s2, 0, 0, dp);
+	}
+	public static int LCSRECMEMOHelper(String str1, String str2, int i, int j, int[][] dp) {
+		if(i == str1.length() || j == str2.length()) {
+			return 0;
+		}
+
+		int myAns;
+		if(str1.charAt(i) == str2.charAt(j)) {
+			int smallAns;
+			if(dp[i + 1][j + 1] == -1) {
+				smallAns = LCSRECMEMOHelper(str1, str2, i + 1, j + 1, dp);
+				dp[i + 1][j + 1] = smallAns;
+			}else {
+				smallAns = dp[i + 1][j + 1];
+			}
+			myAns = 1 + smallAns;
+		}else {
+			int ans1, ans2;
+			if(dp[i + 1][j] == -1) {
+				ans1 = LCSRECMEMOHelper(str1, str2, i + 1, j, dp);
+				dp[i + 1][j] = ans1;
+			}else {
+				ans1 = dp[i + 1][j];
+			}
+			
+			if(dp[i][j + 1] == -1) {
+				ans2 = LCSRECMEMOHelper(str1, str2, i, j + 1, dp);
+				dp[i][j + 1] = ans2;
+			}else {
+				ans2 = dp[i][j + 1];
+			}
+			
+			myAns = Math.max(ans1, ans2);
+		}
+		return myAns;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		String str1 = s.nextLine();
 		String str2 = s.nextLine();
-		System.out.println(lcs(str1, str2));
+		//		System.out.println(lcs(str1, str2));
+		int[][] dp = new int[(str1.length() + 1)][str2.length() + 1];
+		for(int i = 0; i < dp.length; i++) {
+			for(int j = 0; j < dp[0].length; j++) {
+				dp[i][j] = -1;
+			}
+		}
+		System.out.println(LCSRECMEMO(str1, str2, dp));
 	}
 
 }
