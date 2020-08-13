1082b7f4bac34a70df819675352d1140436e75ed
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-13 21:23:07 +0530

    done with knapsack

--- dynamicProgramming/KnapSack01.java
+++ dynamicProgramming/KnapSack01.java
@@ -50,6 +50,27 @@ public class KnapSack01 {
 		}
 		return ans;
 	}
+	
+	public static int knapsackIter(int W, int val[], int wt[]) {
+		
+		int n = val.length;
+		int[][] dp = new int[n + 1][W+ 1];
+		
+		for(int i = n - 1; i >= 0; i--) {
+			for(int w = 0; w <= W; w++) {
+				int ans;
+				if(wt[i] <= w) {
+					int ans1 = val[i] + dp[i + 1][w-wt[i]];
+					int ans2 = dp[i + 1][w];
+					ans = Math.max(ans1, ans2);
+				}else {
+					ans = dp[i + 1][w];
+				}
+				dp[i][w] = ans;
+			}
+		}
+		return dp[0][W];
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
@@ -63,7 +84,8 @@ public class KnapSack01 {
 			value[i] = s.nextInt();
 		}
 		int maxWeight = s.nextInt();
-		System.out.println(knapsackRec(weight, value, maxWeight, n));
+//		System.out.println(knapsackRec(weight, value, maxWeight, n));
+		System.out.println(knapsackIter(maxWeight, value, weight));
 	}
 
 }
